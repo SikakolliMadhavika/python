b5d6c15880e21566a4b71e7365948b3a5a38d297
SikakolliMadhavika <madhavikasikakolli24@gmail.com>
2025-01-21 17:52:27 +0530

    basics of python

---
+++ basic.py
@@ -0,0 +1,62 @@
+# Arithmatic operations
+print(123 + 456)
+print(123 - 456)
+print(123 * 456)
+print(123 / 456)
+print(123 // 456)
+
+
+#using variables
+a=123
+b=456
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(4**4)
+
+
+#strings
+a='Madhavika'
+
+
+# length of str
+print(len(a))
+
+
+# length of str
+print(type(a))
+
+
+#indexing
+print(a[7])
+print(a[0])
+print(a[3:7])
+
+
+# if else elif condition
+x=2
+if x>4:
+    print('x is greater than 4')
+elif x==2:
+    print('x is 2')
+else:
+    print('x is less than 4')
+
+
+# for loop
+for x in range(2,5):
+    print('Hii')
+
+
+# While loop
+i=3
+while i>4 :
+    print('Hello')
+    i+=1
+
+
+
+
+
